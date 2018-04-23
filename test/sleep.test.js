e5d9d1a2345edac111a8b52607a7bd44bdc6077d
Kamil <kamil@m>
2018-04-23 23:24:37 +0200

    Refactored sleep.test.js, removed chai and used standart assert node library.

--- test/sleep.test.js
+++ test/sleep.test.js
@@ -1,138 +1,123 @@
-var expect = require('chai').expect;
-var sleep = require('../sleep-async')();
+const sleep = require('../sleep-async')();
+const assert = require('assert');
 
-describe('sleep.test', function() {
-  before(function (done) {
-    done();
-  });
+describe('Sleep tests:', function () {
 
-  describe('Sleep tests', function () {
-
-    describe('When sleep with timeout.', function () {
-      var startTime = null;
-      var stopTime = null;
-      before(function (done) {
-        startTime = new Date().getTime();
-        sleep.sleep(500, function(){
-          stopTime = new Date().getTime();
-          done();
+  describe('When sleep with timeout', function () {
+    it('then should wake up after sleep time.', async function () {
+      const startTime = new Date().getTime();
+      
+      const stopTime = await new Promise(function(resolve, reject){
+        sleep.sleep(500, function createPromise(){
+          resolve(new Date().getTime());
         });
       });
 
-      it('Should wake up after sleep time.', function () {
-        var difference = stopTime-startTime;
-        expect(difference).to.be.greaterThan(500).and.lessThan(600);
-      });
-
+      const difference = stopTime-startTime;
+      assert.ok(difference >= 500 && difference <= 600);
     });
 
-    describe('When sleep with condition.', function () {
-      var startTime = null;
-      var stopTime = null;
-      var collection = [];
-      before(function (done) {
-        startTime = new Date().getTime();
+  });
+
+  describe('When sleep with condition', function () {
+    it('then should wake up when condition is true.', async function () {
+
+      let collection = [];
+      const startTime = new Date().getTime();
+
+      const stopTime = await new Promise(function createPromise(resolve, reject){
         sleep.sleepWithCondition(
-          function(){
-            var conditionResult = collection.length > 97;
+          function condition(){
+            const conditionResult = collection.length > 97;
             return conditionResult;
           },
-          500,
-          function(){
-            stopTime = new Date().getTime();
-            done();
-          });
-        for(var i = 0; i < 100; i++){
+          500, 
+          function done(){
+            resolve(new Date().getTime());
+        });
+
+        for(let i = 0; i < 100; i++){
           collection.push(i);
         }
       });
 
-      it('Should wake up when condition is true.', function () {
-        var difference = stopTime-startTime;
-        expect(difference).to.be.lessThan(500);
-      });
-
+      const difference = stopTime-startTime;
+      assert.ok(difference < 500); 
     });
 
-    describe('When sleep with false condition.', function () {
-      var startTime = null;
-      var stopTime = null;
-      before(function (done) {
-        startTime = new Date().getTime();
+  });
+
+  describe('When sleep with false condition', function () {
+    it('then should wake up after sleep time.', async function () {
+
+      const startTime = new Date().getTime();
+      const stopTime = await new Promise(function createPromise(resolve, reject){
         sleep.sleepWithCondition(
           function(){
             return true == false;
           },
           500,
-          function(){
-            stopTime = new Date().getTime();
-            done();
-          });
+          function done(){
+            resolve(new Date().getTime());
+        });
       });
 
-      it('Should wake up after sleep time.', function () {
-        var difference = stopTime-startTime;
-        expect(difference).to.be.greaterThan(500).and.lessThan(600);
-      });
-
-    });
-
-    describe('When sleep with false condition.', function () {
-      var startTime = null;
-      var stopTime = null;
-      before(function (done) {
-
-        var options = {
-          sleep: 500
-        };
-
-        startTime = new Date().getTime();
-        sleep.sleepWithCondition(
-          function(){
-            return true == false;
-          },
-          options,
-          function(){
-            stopTime = new Date().getTime();
-            done();
-          });
-      });
-
-      it('Should wake up after sleep time options.', function () {
-        var difference = stopTime-startTime;
-        expect(difference).to.be.greaterThan(500).and.lessThan(600);
-      });
-
-    });
-
-    describe('When sleep with condition and interval has changed.', function () {
-      var startTime = null;
-      var stopTime = null;
-      before(function (done) {
-
-        var options = {
-          interval: 1000,
-          sleep: 500
-        };
-
-        startTime = new Date().getTime();
-        sleep.sleepWithCondition(
-          function(){
-            return true == false;
-          },
-          options,
-          function(){
-            stopTime = new Date().getTime();
-            done();
-          });
-      });
-
-      it('Should wake up after sleep time with interval options.', function () {
-        var difference = stopTime-startTime;
-        expect(difference).to.be.greaterThan(1000).and.lessThan(1500);
-      });
-
+      const difference = stopTime-startTime;
+      assert.ok(difference >= 500 && difference <= 600);
     });
 
   });
+
+  describe('When sleep with false condition', function () {
+    it('then should wake up after sleep time options.', async function () {
+      
+      const options = {
+        sleep: 500
+      };
+
+      const startTime = new Date().getTime();
+      const stopTime = await new Promise(function createPromise(resolve, reject) {
+        sleep.sleepWithCondition(
+          function(){
+            return true === false;
+          },
+          options,
+          function done(){
+            resolve(new Date().getTime());
+        });
+      });
+
+      const difference = stopTime-startTime;
+      assert.ok(difference >= 500 && difference <= 600);
+    });
+
+  });
+
+  describe('When sleep with condition and interval has changed', function () {
+    it('then should wake up after sleep time with interval options.', async function () {
+
+      const options = {
+        interval: 1000,
+        sleep: 500
+      };
+
+      const startTime = new Date().getTime();
+      
+      const stopTime = await new Promise(function createPromise(resolve, reject) {
+        sleep.sleepWithCondition(
+          function(){
+            return true === false;
+          },
+          options,
+          function done(){
+            resolve(new Date().getTime());
+        });
+      });
+
+      const difference = stopTime-startTime;
+      assert.ok(difference >= 1000 && difference <= 1500);
+    });
+
+  });
+
 });
